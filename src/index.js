19baf6267a04b4a5e1666a7f7886b0c5fe811c35
Patrick Smith <patrick@burntcaramel.com>
2018-01-09 00:24:28 +1100

    Fix case-sensitive typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-console.log('load')
-
-import('./main.elm').then(Elm => {
+import('./Main.elm').then(Elm => {
   var mountNode = document.getElementById('app')
   var app = Elm.Main.embed(mountNode)
 })
