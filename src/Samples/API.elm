85e5c06397b51b47af27e006f1f4e82650fca4b6
Patrick Smith <patrick@burntcaramel.com>
2018-02-07 00:26:47 +1100

    Simplify API example

--- src/Samples/API.elm
+++ src/Samples/API.elm
@@ -3,32 +3,14 @@ module Samples.API exposing (source)
 
 source : String
 source =
-    """# Load from API
+    """# Welcome to Datadown
 
-## data
+## post
 <https://jsonplaceholder.typicode.com/posts/3>
 
-## out
-```
-data
-```
-
-## render
+## view
 ```html
-<h2>{{ data }}</h2>
+<p>{{ post.title }}</p>
 ```
-
-
-
-## data
-### post1: json
-https://jsonplaceholder.typicode.com/posts/1
-
-### post2: json
-https://jsonplaceholder.typicode.com/posts/2
-
-## render
-```html
-<h2>{{ post1.title }}</h2>
 ```
 """
