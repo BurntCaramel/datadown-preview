a27bbfd545fe319646e3827904ebb41e9d84fe44
Patrick Smith <patrick@burntcaramel.com>
2018-02-17 04:23:32 +1100

    Add dumb parsing of index from URL

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import Elm from './Main.elm'
 import './main.css'
 
+function parseIntOr(input, fallback) {
+  const int = parseInt(input, 10)
+  if (isNaN(int)) {
+    return fallback
+  }
+  return int
+}
+
 const query = location.search.slice(1).split('&').reduce((pairs, s) => {
   const [key, value] = s.split('=')
   pairs[key] = value
   return pairs
 }, {})
 
-function parseIntOrNull(input) {
-  const int = parseInt(input, 10)
-  if (isNaN(int)) {
-    return null
-  }
-  return int
-}
+const currentDocumentIndex = parseIntOr(location.pathname.split('/').slice(-1), 0) - 1
 
 const mountNode = document.getElementById('app')
 const app = Elm.Main.embed(mountNode, {
-  editModeInt: parseIntOrNull(query['editMode']),
-  currentDocumentIndex: 0
+  editModeInt: parseIntOr(query['editMode'], null),
+  currentDocumentIndex
 })
